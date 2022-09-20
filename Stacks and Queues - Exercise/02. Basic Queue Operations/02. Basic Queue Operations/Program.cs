eb83cb5d6370b8c7b45a9a370d5e86c5343d5cdd
venci0003 <venci0003@gmail.com>
2022-09-20 22:01:08 +0300

    Uploaded solutions to Stacks and Queues - Exercise

---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int countOfElementsToBeQueued = commands[0];
+            int countOfElementsToBeDequeued = commands[1];
+            int elementToLookFor = commands[2];
+
+            int[] numbersInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < countOfElementsToBeQueued; i++)
+            {
+                numbers.Enqueue(numbersInput[i]);
+            }
+
+            for (int i = 0; i < countOfElementsToBeDequeued; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (numbers.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine($"{numbers.Min()}");
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/09. Simple Text Editor/09. Simple Text Editor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+            StringBuilder stringToBeManipulated = new StringBuilder();
+            Stack<string> memory = new Stack<string>();
+            memory.Push(string.Empty);
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+                if (command[0] == "1")
+                {
+                    string wordToBeAppended = command[1];
+                    stringToBeManipulated.Append(wordToBeAppended);
+                    memory.Push(stringToBeManipulated.ToString());
+                }
+                else if (command[0] == "2")
+                {
+                    int charactersToRemove = int.Parse(command[1]);
+                    stringToBeManipulated = stringToBeManipulated.Remove(stringToBeManipulated.Length - charactersToRemove, charactersToRemove);
+                    memory.Push(stringToBeManipulated.ToString());
+                }
+                else if (command[0] == "3")
+                {
+                    int index = int.Parse(command[1]);
+                    if (index >= 0 && index <= stringToBeManipulated.Length)
+                    {
+                        Console.WriteLine(stringToBeManipulated[index - 1]);
+                    }
+                }
+                else if (command[0] == "4")
+                {
+                    memory.Pop();
+                    string previousVersion = memory.Peek();
+                    stringToBeManipulated = new StringBuilder(previousVersion);
+                }
+            }
+        }
+    }
+}
