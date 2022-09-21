c7761ddceb0c6ebf8d94b6f4aa1c7118e86b63a9
venci0003 <venci0003@gmail.com>
2022-09-21 13:36:22 +0300

    Uploaded solutions to Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int cols = matrixSize;
+            int rows = matrixSize;
+
+            int[,] matrix = new int[cols, rows];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string rowsData = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowsData[col];
+                }
+            }
+            char targetSymbol = char.Parse(Console.ReadLine());
+            for (int row = 0; row < rows; row++)
+            {           
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row,col] == targetSymbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{targetSymbol} does not occur in the matrix");
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _5._Square_With_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int subMatrixRows = 2;
+            int subMatrixCols = 2;
+            string[] sizes = Console.ReadLine().Split(", ");
+            int rows = int.Parse(sizes[0]);
+            int cols = int.Parse(sizes[1]);
+
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowData = Console.ReadLine().Split(", ");
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(rowData[col]);
+                }
+            }
+
+            int maxSum = 0;
+            int squareStartRow = 0;
+            int squareStartCol = 0;
+            for (int row = 0; row < rows - subMatrixRows + 1; row++)
+            {
+                for (int col = 0; col < cols - subMatrixCols + 1; col++)
+                {
+                    int sum = 0;
+                    sum += matrix[row, col];
+                    sum += matrix[row + 1, col];
+                    sum += matrix[row, col + 1];
+                    sum += matrix[row + 1, col + 1];
+                    if (sum > maxSum)
+                    {
+                        squareStartRow = row;
+                        squareStartCol = col;
+                        maxSum = sum;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[squareStartRow,squareStartCol]} {matrix[squareStartRow,squareStartCol + 1]}");
+            Console.WriteLine($"{matrix[squareStartRow + 1, squareStartCol]} {matrix[squareStartRow + 1, squareStartCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
