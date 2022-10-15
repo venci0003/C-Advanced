6c7df59c5d0518cbb2503c1d5f3ff4749818de5f
venci0003 <venci0003@gmail.com>
2022-10-15 19:59:57 +0300

    Uploaded solutions to - CSharp Advanced Retake Exam - 13 April 2022

---
+++ C# Advanced - Exams/CSharp Advanced Retake Exam - 13 April 2022/02. Truffle Hunter/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Truffle_Hunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] matrix = ReadMatrixInformation();
+
+            int blackTruffleCount = 0;
+
+            int summerTruffleCount = 0;
+
+            int whiteTruffleCount = 0;
+
+            int trufflesEatenByTheBoar = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Stop the hunt")
+                {
+                    break;
+                }
+
+                string[] token = command.Split();
+
+                if (token[0] == "Collect")
+                {
+                    int row = int.Parse(token[1]);
+
+                    int col = int.Parse(token[2]);
+
+                    if (isValid(row, col, matrix))
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            matrix[row, col] = '-';
+                            blackTruffleCount++;
+                        }
+                        else if (matrix[row, col] == 'S')
+                        {
+                            matrix[row, col] = '-';
+                            summerTruffleCount++;
+                        }
+                        else if (matrix[row, col] == 'W')
+                        {
+                            matrix[row, col] = '-';
+                            whiteTruffleCount++;
+                        }
+                    }
+                }
+                else if (token[0] == "Wild_Boar")
+                {
+
+                    int rowIndex = int.Parse(command.Split().Skip(1).First());
+
+                    int colIndex = int.Parse(command.Split().Skip(2).First());
+
+                    string direction = command.Split().Last();
+
+                    while (isValid(rowIndex, colIndex, matrix))
+                    {
+                        if ((new char[] { 'B', 'S', 'W' }).Contains(matrix[rowIndex, colIndex]))
+                        {
+                            trufflesEatenByTheBoar++;
+                            matrix[rowIndex, colIndex] = '-';
+                        }
+                        switch (direction)
+                        {
+                            case "up": rowIndex -= 2; break;
+                            case "down": rowIndex += 2; break;
+                            case "left": colIndex -= 2; break;
+                            case "right": colIndex += 2; break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Peter manages to harvest {blackTruffleCount} black, {summerTruffleCount} summer, and {whiteTruffleCount} white truffles.");
+
+            Console.WriteLine($"The wild boar has eaten {trufflesEatenByTheBoar} truffles.");
+
+            PrintMatrix(matrix);
+        }
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool isValid(int row, int col, char[,] matrix)
+        {
+            if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static char[,] ReadMatrixInformation()
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            for (int row = 0; row < size; row++)
+            {
+                char[] colsInput = string.Join("", Console.ReadLine().Split()).ToCharArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = colsInput[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ C# Advanced - Exams/CSharp Advanced Retake Exam - 13 April 2022/03. Zoo/Zoo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        private readonly List<Animal> animals;
+
+        public List<Animal> Animals { get { return this.animals; } }
+
+        public Zoo(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.animals = new List<Animal>();
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int capacity;
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public string AddAnimal(Animal animal)
+        {
+            if (String.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+            if (!new List<string> { "herbivore", "carnivore" }.Contains(animal.Diet))
+            {
+                return "Invalid animal diet.";
+            }
+            if (animals.Count >= this.Capacity)
+            {
+                return "The zoo is full.";
+            }
+
+            this.animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            return this.animals.RemoveAll(a => a.Species == species);
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            return this.animals.FindAll(a => a.Diet == diet);
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            return this.animals.Find(a => a.Weight == weight);
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int count = this.animals.Where(a => a.Length >= minimumLength && a.Length <= maximumLength).Count();
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+
+    }
+}
