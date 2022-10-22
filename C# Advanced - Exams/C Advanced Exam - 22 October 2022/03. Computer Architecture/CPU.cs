f3790f32bd4c8076f8e2d543a0c25ca5b7c2971b
venci0003 <venci0003@gmail.com>
2022-10-22 21:22:19 +0300

    Uploaded solutions to my Advanced Exam on Saturday October 22 2022

---
+++ C# Advanced - Exams/C Advanced Exam - 22 October 2022/03. Computer Architecture/CPU.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        public CPU(string brand, int cores, double frequency)
+        {
+            this.Brand = brand;
+            this.Cores = cores;
+            this.Frequency = frequency;
+        }
+
+        public string Brand { get; set; }
+        public int Cores { get; set; }
+        public double Frequency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"{this.Brand} CPU:");
+
+            result.AppendLine($"Cores: {this.Cores}");
+
+            result.AppendLine($"Frequency: {this.Frequency:f1} GHz");
+
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C# Advanced - Exams/C Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+
+        public Computer(string model, int capacity)
+        {
+            this.Multiprocessor = new List<CPU>();
+            this.Model = model;
+            this.Capacity = capacity;
+        }
+
+        public List<CPU> Multiprocessor { get; set; }
+        public string Model { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count { get => Multiprocessor.Count; }
+
+        public void Add(CPU cpu)
+        {
+            if (this.Capacity - this.Multiprocessor.Count > 0)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            CPU removeByBrand = this.Multiprocessor.FirstOrDefault(a => a.Brand == brand);
+
+            if (removeByBrand != null)
+            {
+                Multiprocessor.Remove(removeByBrand);
+                return true;
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            return this.Multiprocessor.OrderByDescending(m => m.Frequency).First();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            CPU returnBrand = this.Multiprocessor.FirstOrDefault(a => a.Brand == brand);
+
+            if (returnBrand != null)
+            {
+                return returnBrand;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"CPUs in the Computer {Model}:");
+            foreach (CPU item in this.Multiprocessor)
+            {
+                report.AppendLine(item.ToString());
+            }
+            return report.ToString().Trim();
+        }
+    }
+}
