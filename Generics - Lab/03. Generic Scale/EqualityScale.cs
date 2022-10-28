ad0d887d5b6bc7922d078e344cd8c9e3e4d43e96
venci0003 <venci0003@gmail.com>
2022-10-28 22:51:54 +0300

    Uploaded solutions to - Generics - Lab

---
+++ Generics - Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._Generic_Scale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public bool AreEqual()
+        {
+            return this.left.Equals(this.right);
+        }
+    }
+}
